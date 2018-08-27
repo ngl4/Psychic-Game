a2cc6b32612c4aa71550d9f464329c364aa2c55c
Binh Lam <cindybinh444@gmail.com>
2018-08-27 12:39:35 -0400

    Edit comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,5 @@
-//Problem: Computer Guess has store all the letter info
-// and the Computer will be randomly thiinking/choosing a letter
 
-//Solution: Create a variable and an Array containning all the alphabet letters
-//mathfloor/ mathrandom * option.length
-
+//Declare a global array variable containing alphabet letters
 var computerChoices = [
   "a",
   "b",
@@ -33,136 +29,45 @@ var computerChoices = [
   "z"
 ];
 
-// Randomly chooses a choice from the options array. This is the Computer's guess.
-
-
-//Problem: Computer has a letter guessed before user presses the key
-
-//Problem: user guesses will be based on keypress
-//Solution: Create a function for this key pressed with an Event Listenner of .onkeyup
-//its data will be stored in a variable for future access
-
-var winNum = 0; 
+//Declare global variables 
+var winNum = 0;
 var loseNum = 0;
 var guessLeft = 9;
-
 var userGuessArr = [];
 
-
+//A function reacts to every key presses
 document.onkeyup = function(event) {
-  
-    
- 
 
-    var userGuess = event.key;
+//Declare local variables to save both keypress and random computer generated number
+  var userGuess = event.key;
 
-     var computerGuess =
+  var computerGuess =
     computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
+//Every key letter users press will be pushed to the userGuessArr for displaying in HTML at 'user-guess' Id
+  userGuessArr.push(userGuess);
+  document.getElementById("user-guess").innerHTML = userGuessArr.join(", ");
 
-
-    userGuessArr.push(userGuess);
-    document.getElementById("user-guess").innerHTML = userGuessArr.join(", ");
-   
-
-
-
-  //Conditionals!!!!
-
-  //Problems: Wins
-  //When the player wins, increase the Wins counter and start the game over again (without refreshing the page)
-  
-
- 
-
-  //Problem: Losses
-  //When the player loses, increase the Losses counter and restart the game without a page refresh (just like when the user wins).
-
-  
-
-  //Problem: Guesses Left
-  //There are only 9 guesses
-  //Every time the key presess, it counts as one guess
-  //The display will show one guess less, which is 8
-  
-
-    if (userGuess === computerGuess) {
-
-
-        guessLeft = 9; 
-        winNum += 1;
-        userGuessArr = [];
-
-    
-
-    }else if (userGuess !== computerGuess) {
-
-        guessLeft -= 1;
-        console.log(guessLeft);
-        
-
-        if (guessLeft === 0) {
-
-            guessLeft = 9;
-            loseNum += 1;
-            userGuessArr = [];
-            
-        }
-
+  //If-else if conditionals for the comparison logic between userGuess and computerGuess
+  //When user gets the correct guess, game gets reset except the win number (goes up by 1)
+  if (userGuess === computerGuess) { 
+    guessLeft = 9;
+    winNum += 1;
+    userGuessArr = [];
+  } else if (userGuess !== computerGuess) {
+    guessLeft -= 1;
+    console.log(guessLeft);
+//When user gets the wrong guess, game gets reset except for the lose number (goes up by 1)
+    if (guessLeft === 0) {
+      guessLeft = 9;
+      loseNum += 1;
+      userGuessArr = [];
     }
+  }
+//Display all the numbers at the specific area in HTML 
+  document.getElementById("win-num").textContent = winNum;
 
- 
-    document.getElementById("win-num").textContent = winNum;
+  document.getElementById("lose-num").textContent = loseNum;
 
-    document.getElementById("lose-num").textContent = loseNum;
-
-    document.getElementById("guess-left").textContent = guessLeft;
-
-
-
-   
-
-
-  //Problem: You Guesses so far
-  //Every key presses will display
-  //The display will show a letter on the "You Guesses so far"
-  //once it reaches nine
-  //Display these until the user either wins or loses
+  document.getElementById("guess-left").textContent = guessLeft;
 };
-
-
-
-
-
-//Trial and Error====================================================
-
-// var guessLeft = 9;
-// var guessLeft = document.getElementById("guess-left").innerHTML;
-
-// for (guessLeft; guessLeft > -1; guessLeft--) {
-//   if (userGuess !== computerGuess) {
-//     document.getElementById("guess-left").innerHTML = guessLeft;
-
-//     console.log(guessLeft);
-
-//     if (guessLeft === 0) {
-//       return loseNum += 1;
-//       document.getElementById("lose-num").innerHTML = loseNum;
-//     }
-//   } else {
-//     return guessLeft = 9;
-//     document.getElementById("guess-left").innerHTML = guessLeft;
-
-//     return winNum += 1;
-//     document.getElementById("win-num").innerHTML = winNum;
-//   }
-// }
-
-// var html =
-                 
-// "<p>Wins: " + winNum + "</p>" +
-// "<p>Losses: " + loseNum + "</p>" +
-// "<p>Guesses Left: " + guessLeft + "</p>" +
-// "<p>You Guesses so far: " + userGuessArr.join(", ") + "</p>" ;
-
-// document.querySelector("#game").innerHTML = html;
